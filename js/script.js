5b899fd7ac8d067151020b3faf3e6c616353ed3e
GigaGigot <theolassaube@gmail.com>
2022-03-27 18:53:21 +0200

    Trying to fix cookies v3

--- js/script.js
+++ js/script.js
@@ -1093,7 +1093,7 @@ function getCookie(name) {
 };
 
 function checkCookie(name) {
-    let save = getCookie();
+    let save = getCookie(name);
     if ((save || {}) != {}) {
         load(save);
         return true;
